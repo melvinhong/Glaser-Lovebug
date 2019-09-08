ebc007604001d8b7699aa80bbb679a1836d92e37
s3678831 - JunwangL <41616180+JunwangL@users.noreply.github.com>
2019-09-09 07:22:25 +1000

    Added login function for both admin and users

--- app/User.php
+++ app/User.php
@@ -36,4 +36,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+      return $this->belongsToMany('App\Role');
+    }
+
+    public function hasAnyRoles($roles)
+    {
+      return null != $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasAnyRole($role)
+    {
+      return null != $this->roles()->where('name', $role)->first();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', function () {
     return view('admin.admin');
-});
+})->middleware(['auth', 'auth.admin']);
