8305bf927d8222d8958bcf0b13585fc60e5def40
s3678831 - JunwangL <41616180+JunwangL@users.noreply.github.com>
2019-09-12 13:42:28 +1000

    Minor changes on admin page. (#3)
    * Added login function for both admin and users
    
    * Minor changes on admin page

--- resources/views/layouts/adminpage.blade.php
+++ resources/views/layouts/adminpage.blade.php
@@ -106,7 +106,12 @@
                   </p>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Sign Out</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">Sign Out</a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
                 </div>
               </li>
             </ul>
