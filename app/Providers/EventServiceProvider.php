c499469a44edbe6c963c9941955625b203b2e504
Fixatedwings <1204680369@qq.com>
2019-09-19 17:45:19 +1000

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,6 +17,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+            
+        'App\Events\Event' => [
+            'App\Listeners\EventListener',
         ],
     ];
 
