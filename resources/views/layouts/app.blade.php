5472310768e47c82a7111d7309fe550e84c27bb6
JunwangL <41616180+JunwangL@users.noreply.github.com>
2019-10-10 14:55:38 +1100

    Added 'Impersonate' functionaliy for admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,17 +87,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                  <!-- <a class="dropdown-item" href="{{ url('/profile') }}">
-                                      {{ __('My Profile') }}
-                                  </a>
 
-                                  <a class="dropdown-item" href="{{ url('/settings') }}">
-                                      {{ __('Settings') }}
+                                  <a class="dropdown-item" href="{{ route('admin.impersonate.destroy') }}">
+                                      <i class="fa fa-btn fa-times-rectangle"></i>
+                                      {{ __('Stop Impersonating') }}
                                   </a>
-
-                                  <a class="dropdown-item" href="{{ url('/chatpage') }}">
-                                      {{ __('Messenger') }}
-                                  </a> -->
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
